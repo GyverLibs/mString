ff909b0f8077af3fe9ca0fbc9c8b653b80d3799d
Alex <alexgyvershow@ya.ru>
2022-08-31 10:35:53 +0300

    upd

--- src/utils.h
+++ src/utils.h
@@ -1,5 +1,7 @@
 #ifndef _MS_UTILS_h
 #define _MS_UTILS_h
+#include <Arduino.h>
+
 char* mUtoa(uint32_t value, char *buffer, bool clear = 1);
 char* mLtoa(int32_t value, char *buffer, bool clear = 1);
 char* mFtoa(double value, int8_t decimals, char *buffer);
